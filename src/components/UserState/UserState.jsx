f2ada54b8f3056e68fa9195da679afa73cb3a468
Edwin Ortiz <edarcode@gmail.com>
2024-06-19 07:16:21 -0500

    modificando estilos UserState

--- src/components/UserState/UserState.jsx
+++ src/components/UserState/UserState.jsx
@@ -1,13 +1,17 @@
 import { ACTIVE, INACTIVE } from "../../constants/states.js";
+import CheckCircle from "../icons/CheckCircle.jsx";
+import CrossCircle from "../icons/CrossCircle.jsx";
 import css from "./UserState.module.css";
 
-const UserState = ({ state }) => {
-	const classNameState = state ? css.active : css.inactive;
-	return (
-		<p className={`${css.state} ${classNameState}`}>
-			{state ? ACTIVE : INACTIVE}
-		</p>
-	);
-};
+export default function UserState({ state }) {
+	const classState = state ? css.active : css.inactive;
+	const textState = state ? ACTIVE : INACTIVE;
+	const Icon = state ? CheckCircle : CrossCircle;
 
-export default UserState;
+	return (
+		<div className={`${css.state} ${classState}`}>
+			<Icon />
+			<p>{textState}</p>
+		</div>
+	);
+}
