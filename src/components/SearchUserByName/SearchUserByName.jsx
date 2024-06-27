f4471eda3ffb542ac97f29a39e3931c4968058f0
Edwin Ortiz <edarcode@gmail.com>
2024-06-27 10:32:09 -0500

    agregando debounce al input de busqueda por nombre

--- src/components/SearchUserByName/SearchUserByName.jsx
+++ src/components/SearchUserByName/SearchUserByName.jsx
@@ -1,12 +1,29 @@
-import { useGetUsers } from "../../hooks/useGetUsers.jsx";
+import { useEffect, useRef } from "react";
 import { useUsers } from "../../stores/users/useUsers.jsx";
 import InputSearch from "../forms/InputSearch/InputSearch.jsx";
 
 export default function SearchUserByName() {
 	const userToSearch = useUsers(users => users.userToSearch);
 	const changeUserToSearch = useUsers(users => users.changeUserToSearch);
+	const getUsers = useUsers(users => users.getUsers);
+	const isFirstRender = useRef(true);
 
-	useGetUsers(userToSearch);
+	useEffect(() => {
+		if (isFirstRender.current) {
+			isFirstRender.current = false;
+			return;
+		}
+
+		const controller = new AbortController();
+		const timeoutId = setTimeout(() => {
+			getUsers({ signal: controller.signal });
+		}, 500);
+
+		return () => {
+			clearTimeout(timeoutId);
+			controller.abort();
+		};
+	}, [userToSearch, getUsers]);
 
 	return (
 		<InputSearch
