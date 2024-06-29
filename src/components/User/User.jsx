1a05bb136f9d947a176ea45252ddd59f145c591b
Edwin Ortiz <edarcode@gmail.com>
2024-06-29 14:06:48 -0500

    lista de usuarios responsive

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,4 +1,5 @@
 import { useCurrentUserForm } from "../../stores/current-user-form/useCurrentUserForm.jsx";
+import { addAllClassName } from "../../utils/addAllClassName.js";
 import UserProfile from "../UserProfile/UserProfile.jsx";
 import UserRole from "../UserRole/UserRole.jsx";
 import UserState from "../UserState/UserState.jsx";
@@ -7,13 +8,19 @@ import Pencil from "../icons/Pencil.jsx";
 import Trash from "../icons/Trash.jsx";
 import css from "./css.module.css";
 
-export default function User(user) {
+export default function User({ className, isUserGrid, ...user }) {
 	const setFormToEdit = useCurrentUserForm(state => state.setFormToEdit);
 	const setFormToDelete = useCurrentUserForm(state => state.setFormToDelete);
 	const { name, username, state, role } = user;
 
+	const finalClassName = addAllClassName([
+		css.user,
+		className,
+		isUserGrid && css.userGrid
+	]);
+
 	return (
-		<article className={css.user}>
+		<article className={finalClassName}>
 			<UserProfile className={css.profile} name={name} username={username} />
 			<UserRole role={role} />
 			<UserState state={state} />
--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -1,17 +1,53 @@
+import { useState } from "react";
 import { useUsers } from "../../stores/users/useUsers.jsx";
+import { addAllClassName } from "../../utils/addAllClassName.js";
 import User from "../User/User.jsx";
+import Grid from "../icons/Grid.jsx";
+import Lines from "../icons/Lines.jsx";
 import css from "./css.module.css";
 const UserList = () => {
 	const allUsers = useUsers(users => users.allUsers);
 	const loading = useUsers(users => users.loading);
 	const err = useUsers(users => users.err);
+	const [isUserGrid, setIsUserGrid] = useState(false);
 
 	if (loading) return <p>Cargando usuarios...</p>;
 	if (err) return <p className={css.err}>Err al cargar usuarios 😢</p>;
 	if (allUsers.length <= 0) return <p>No hay usuarios</p>;
 
-	const usersRendered = allUsers.map(user => <User key={user.id} {...user} />);
-	return <section className={css.list}>{usersRendered}</section>;
+	const users = allUsers.map(user => (
+		<User key={user.id} isUserGrid={isUserGrid} {...user} />
+	));
+
+	const finalClassName = addAllClassName([
+		css.list,
+		isUserGrid && css.listUserGrid
+	]);
+	const finalClassNameGrid = addAllClassName([
+		css.grid,
+		isUserGrid && css.gridActive
+	]);
+
+	const finalClassNameLines = addAllClassName([
+		css.lines,
+		!isUserGrid && css.linesActive
+	]);
+
+	return (
+		<section className={finalClassName}>
+			<div className={css.viewMode}>
+				<Grid
+					className={finalClassNameGrid}
+					onClick={() => setIsUserGrid(true)}
+				/>
+				<Lines
+					className={finalClassNameLines}
+					onClick={() => setIsUserGrid(false)}
+				/>
+			</div>
+			{users}
+		</section>
+	);
 };
 
 export default UserList;
